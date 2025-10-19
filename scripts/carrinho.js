6fb51fda79acd2aedc36ddec1b07fd23fbf55678
miguelucuepandi <miguel.ucuepandi@gmail.com>
2025-10-19 22:41:46 +0100

    Apaguei tudo

--- scripts/carrinho.js
+++
@@ -1,9 +0,0 @@
-const btnLimparCarrinho = document.querySelector(".btnLimparCarrinho");
-
-btnLimparCarrinho.addEventListener("click", (evt) => {
-    evt.preventDefault()
-    let resp = window.confirm("Deseja limpar o carrinho?")
-    if (resp) {
-        alert("Carrinho limpo com sucesso!")
-    }
-})
